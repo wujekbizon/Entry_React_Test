6421068afe5de418770ad4d859026629c2fbb29f
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-10-08 07:20:29 +0200

    add error page content

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import './Error.scss';
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
 
 export class Error extends Component {
   render() {
-    return <div>Error</div>;
+    return (
+      <main className="error-page">
+        <div className="wrrapper">
+          <img src={img} alt="not found" />
+          <h3>Ohh! Page Not Found</h3>
+          <p>We can't seem to find the page youe're looking for</p>
+          <Link to="/">back home</Link>
+        </div>
+      </main>
+    );
   }
 }
 
